d968f2c0c48937753b8255c04bfae8a8eb976fa1
mmnndha <mmnndha@MAA-5CG9224W5R.ant.amazon.com>
2021-06-19 17:35:30 +0530

    Create Automation Project

---
+++ Git/src/testcase/Test1.java
@@ -0,0 +1,19 @@
+package testcase;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void TC1()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mmnndha\\Downloads\\selenium\\chromedriver_win32\\chromedriver.exe"); 
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElementById("email").sendKeys("gokulnanda996@rediffmail.com");
+		driver.findElementById("pass").sendKeys("Nanda@2104");
+		driver.quit();
+	}
+	
+}
